01a5f3434205efa26be5d4d42f6bccccb6f99487
Simon Théorêt <simonteoret@hotmail.com>
2025-03-29 17:16:36 -0400

    feat(client): Started the clients interfaces

---
+++ internal/client/client.go
@@ -0,0 +1,23 @@
+package client
+
+import (
+	"fmt"
+	"time"
+)
+
+// This struct contains the data we need from files located on the cloud.
+type FileWithMetadata struct {
+	LastModified time.Time
+	Name         string
+}
+
+// Output of a `ListFiles` call from a `Client`
+type ListedFiles[T fmt.Stringer] struct {
+	Inner []T
+	Files []FileWithMetadata
+}
+
+// Client Interface over what a client must do and return.
+type Client[T fmt.Stringer] interface {
+	ListFiles() ListedFiles[fmt.Stringer]
+}
